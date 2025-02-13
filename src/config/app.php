97b639b791c4b7699a8bbc8ff3e107fb66a4840d
Tony <57773117@qq.com>
2025-02-13 16:21:46 +0800

    定义应用配置文件、视图模版

---
+++ src/config/app.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'debug'              => true,
+    'controller_suffix'  => '',
+    'controller_reuse'   => false,
+    'public_path'        => base_path('public' . DIRECTORY_SEPARATOR . 'web'),
+    'plugin_market_host' => 'https://www.t2engine.cn',
+    'version'            => '1.0.0',
+];
