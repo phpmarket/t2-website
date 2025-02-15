c8e1a38c06a7d73c340612d3ce4d256cc36d2cbe
Tony <57773117@qq.com>
2025-02-16 05:23:40 +0800

    Install.php

--- src/Install.php
+++ src/Install.php
@@ -2,12 +2,7 @@
 
 class Install
 {
-    /**
-     * 常量：IS_PLUGIN
-     * 用于标识当前类是否为 T2Engine 插件
-     * 值为 true，表示是 T2Engine 插件
-     */
-    const IS_PLUGIN = true;
+    const bool IS_INSTALL = true;
 
     /**
      * @var array $pathRelation
